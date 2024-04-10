6302635ffb98511f0731436b4c8193471c31b499
Drixner <drixnerrafael1@gmail.com>
2024-04-09 23:06:37 -0500

    Add crearDeck function and pedirCarta function to juego.js

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos = ["C", "D", "H", "S"];
 const especiales = ["A", "J", "Q", "K"];
 
+//esta funcion crea un nuevo deck
 const crearDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (let tipo of tipos) {
@@ -26,4 +27,30 @@ const crearDeck = () => {
   return deck;
 };
 
+console.warn("El deck origital es :");
 console.log(crearDeck());
+
+//esta funcion me permite tomar una carta y a la vex eliminarla del deck
+
+console.warn("el deck despues de pedir una carta es :");
+const pedirCarta = () => {
+  if (deck.length === 0) {
+    throw "No hay cartas en el deck";
+  }
+  const carta = deck.pop();
+
+  console.log(deck);
+  console.log(carta);
+  return carta;
+};
+
+// pedirCarta();
+
+const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+  // console.log({ valor });
+  return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1; // El método isNaN() en JavaScript se utiliza para determinar si un valor es NaN (Not a Number). Este método devuelve true si el valor es NaN, y false si no lo es.
+};
+
+const valor = valorCarta(pedirCarta());
+console.log({ valor });

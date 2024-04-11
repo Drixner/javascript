9b4cf52de31b4b77c2c40609569075bf6cc0619b
Drixner <drixnerrafael1@gmail.com>
2024-04-10 23:26:14 -0500

    Update juego.js and index.html in 02-blackjack folder

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -9,6 +9,15 @@ let deck = [];
 const tipos = ["C", "D", "H", "S"];
 const especiales = ["A", "J", "Q", "K"];
 
+let puntosJugador = 0,
+  puntosComputadora = 0;
+
+//Referencias del HTML
+
+const btnPedir = document.querySelector("#btnPedir");
+const divCartasJugador = document.querySelector("#jugador-cartas");
+const puntosHTLM = document.querySelectorAll("small");
+
 //esta funcion crea un nuevo deck
 const crearDeck = () => {
   for (let i = 2; i <= 10; i++) {
@@ -24,25 +33,23 @@ const crearDeck = () => {
   }
   // console.log(deck);
   deck = _.shuffle(deck); // Mezcla el deck, de tal manera que no se repitan las cartas
+  console.log(deck);
   return deck;
 };
 
-console.warn("El deck origital es :");
-console.log(crearDeck());
+crearDeck();
 
 //esta funcion me permite tomar una carta y a la vex eliminarla del deck
 
-console.warn("el deck despues de pedir una carta es :");
 const pedirCarta = () => {
   if (deck.length === 0) {
     throw "No hay cartas en el deck";
   }
   const carta = deck.pop();
-
-  console.log(deck);
-  console.log(carta);
   return carta;
 };
+
+pedirCarta();
 
 // pedirCarta();
 
@@ -51,6 +58,10 @@ const valorCarta = (carta) => {
   // console.log({ valor });
   return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1; // El método isNaN() en JavaScript se utiliza para determinar si un valor es NaN (Not a Number). Este método devuelve true si el valor es NaN, y false si no lo es.
 };
+//Eventos
 
-const valor = valorCarta(pedirCarta());
-console.log({ valor });
+btnPedir.addEventListener("click", () => {
+  const carta = pedirCarta();
+  puntosJugador = puntosJugador + valorCarta(carta);
+  puntosHTLM[0].innerText = puntosJugador;
+});

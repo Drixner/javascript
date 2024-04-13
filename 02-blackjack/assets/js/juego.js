02ac426890cf7223c06fd82db634ca29163e7a0a
Drixner <drixnerrafael1@gmail.com>
2024-04-12 22:22:26 -0500

    Update juego.js and index.html in 02-blackjack folder

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -15,7 +15,10 @@ let puntosJugador = 0,
 //Referencias del HTML
 
 const btnPedir = document.querySelector("#btnPedir");
+const btnDetener = document.querySelector("#btnDetener");
+const btnNuevo = document.querySelector("#btnNuevo");
 const divCartasJugador = document.querySelector("#jugador-cartas");
+const divCartasComputadora = document.querySelector("#computadora-cartas");
 const puntosHTLM = document.querySelectorAll("small");
 
 //esta funcion crea un nuevo deck
@@ -58,10 +61,79 @@ const valorCarta = (carta) => {
   // console.log({ valor });
   return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1; // El método isNaN() en JavaScript se utiliza para determinar si un valor es NaN (Not a Number). Este método devuelve true si el valor es NaN, y false si no lo es.
 };
+// turno de la computadora
+
+const turnoComputadora = (puntosMinimos) => {
+  do {
+    const carta = pedirCarta();
+    puntosComputadora = puntosComputadora + valorCarta(carta);
+    puntosHTLM[1].innerText = puntosComputadora; // se le asigna el valor de los puntos a la computadora
+
+    const imgCarta = document.createElement("img"); // se crea un elemento img
+    imgCarta.src = `assets/cartas/${carta}.png`; //se crea la ruta de la imagen
+    imgCarta.classList.add("carta"); // se le agrega una clase al elemento img
+    divCartasComputadora.append(imgCarta); // se agrega la imagen al div de cartas del jugador
+
+    if (puntosMinimos > 21) {
+      break;
+    }
+  } while (puntosComputadora < puntosMinimos && puntosMinimos <= 21);
+
+  setTimeout(() => {
+    if (puntosComputadora === puntosMinimos) {
+      alert("Nadie gana");
+    } else if (puntosMinimos > 21) {
+      alert("Computadora gana");
+    } else if (puntosComputadora > 21) {
+      alert("Jugador gana");
+    } else {
+      alert("Computadora gana");
+    }
+  }, 100);
+};
+
 //Eventos
 
 btnPedir.addEventListener("click", () => {
   const carta = pedirCarta();
   puntosJugador = puntosJugador + valorCarta(carta);
   puntosHTLM[0].innerText = puntosJugador;
+
+  const imgCarta = document.createElement("img"); // se crea un elemento img
+  imgCarta.src = `assets/cartas/${carta}.png`; //se crea la ruta de la imagen
+  imgCarta.classList.add("carta"); // se le agrega una clase al elemento img
+
+  divCartasJugador.append(imgCarta); // se agrega la imagen al div de cartas del jugador
+
+  if (puntosJugador > 21) {
+    console.warn("Perdiste");
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
+  } else if (puntosJugador === 21) {
+    console.warn("21, genial");
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoComputadora(puntosJugador);
+  }
+});
+
+btnDetener.addEventListener("click", () => {
+  btnPedir.disabled = true;
+  btnDetener.disabled = true;
+  turnoComputadora(puntosJugador);
+});
+
+btnNuevo.addEventListener("click", () => {
+  console.clear();
+  deck = [];
+  deck = crearDeck();
+  puntosJugador = 0;
+  puntosComputadora = 0;
+  puntosHTLM[0].innerText = 0;
+  puntosHTLM[1].innerText = 0;
+  divCartasComputadora.innerHTML = "";
+  divCartasJugador.innerHTML = "";
+  btnPedir.disabled = false;
+  btnDetener.disabled = false;
 });

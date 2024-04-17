a0435c787cf0fdfc0502d9a311db60dd0540c8e2
Drixner <drixnerrafael1@gmail.com>
2024-04-16 23:36:40 -0500

    Update index.html with missing closing tag and add script for problema.js

---
+++ 01-fundamentos/assets/js/classes/problema.js
@@ -0,0 +1,52 @@
+const fher = {
+  nombre: "Fernando",
+  edad: 30,
+  imprimir() {
+    console.log(`Nombre: ${this.nombre} - edad: ${this.edad}`);
+  },
+};
+
+const pedro = {
+  nombre: "Pedro",
+  edad: 15,
+  imprimir() {
+    console.log(`Nombre: ${this.nombre} - edad: ${this.edad}`);
+  },
+};
+
+fher.imprimir();
+pedro.imprimir();
+
+/// Solucion con clases
+class Persona {
+  constructor(nombre, edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+
+  imprimir() {
+    console.log(`Nombre: ${this.nombre} - edad: ${this.edad}`);
+  }
+}
+
+const maria = new Persona("Maria", 18);
+
+maria.imprimir();
+
+class Auto {
+  constructor(modelo, anio, color) {
+    this.modelo = modelo;
+    this.anio = anio;
+    this.color = color;
+  }
+  imprimir() {
+    console.log(
+      `Modelo: ${this.modelo}, anio: ${this.anio}, color: ${this.color}  `,
+    );
+  }
+}
+
+const carroToyaota = new Auto("toyota", "1893", "amarillo");
+const carroYaris = new Auto("yaris", 1934, "verde");
+
+carroToyaota.imprimir();

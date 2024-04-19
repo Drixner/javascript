7610a308e935dec4f63f83c587f3fe8d5d4bd54e
Drixner <drixnerrafael1@gmail.com>
2024-04-18 21:54:42 -0500

    Add Persona class and create instances in class.js

--- 01-fundamentos/assets/js/classes/class.js
+++ 01-fundamentos/assets/js/classes/class.js
@@ -1,1 +1,23 @@
+class Persona {
+  constructor(nombre, codigo, frase) {
+    this.nombre = nombre;
+    this.codigo = codigo;
+    this.frase = frase;
+  }
+  quienSoy() {
+    console.log(`soy ${this.nombre} y mi codigo es ${this.codigo}`);
+  }
+  miFrase() {
+    console.log(
+      `el causa: ${this.nombre}, dice que su frase es: "${this.frase}"`,
+    );
+  }
+}
 
+const alberto = new Persona("rodrigo", 435, "morire en mi ley");
+const spiderman = new Persona("peter", 324, "soy el vecino pues causa");
+console.log(alberto);
+console.log(spiderman);
+
+alberto.quienSoy();
+alberto.miFrase();

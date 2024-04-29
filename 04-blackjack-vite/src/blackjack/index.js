3cf52a5b341adbcbe926b7b928d6441a429f7f1f
Drixner <drixnerrafael1@gmail.com>
2024-04-29 17:36:40 -0500

    Refactor index.js to import crearDeck from ./usecases/crear-deck

--- 04-blackjack-vite/src/blackjack/index.js
+++ 04-blackjack-vite/src/blackjack/index.js
@@ -1,4 +1,5 @@
 import _ from "underscore";
+import { crearDeck } from "./usecases/crear-deck";
 
 /**
  * 2C = Two of Clubs
@@ -23,25 +24,6 @@ const divCartasJugador = document.querySelector("#jugador-cartas");
 const divCartasComputadora = document.querySelector("#computadora-cartas");
 
 const puntosHTML = document.querySelectorAll("small");
-
-// Esta función crea un nuevo deck
-const crearDeck = () => {
-  for (let i = 2; i <= 10; i++) {
-    for (let tipo of tipos) {
-      deck.push(i + tipo);
-    }
-  }
-
-  for (let tipo of tipos) {
-    for (let esp of especiales) {
-      deck.push(esp + tipo);
-    }
-  }
-  // console.log( deck );
-  deck = _.shuffle(deck);
-  console.log(deck);
-  return deck;
-};
 
 crearDeck();
 

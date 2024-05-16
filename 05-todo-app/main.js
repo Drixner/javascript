252f8335db143d8cc5fd7dad66287d08c3fb2251
Drixner <drixnerrafael1@gmail.com>
2024-05-15 23:23:04 -0500

    update

--- 05-todo-app/main.js
+++ 05-todo-app/main.js
@@ -1,3 +1,4 @@
 import "./style.css";
+import { App } from "./src/todo/app";
 
-console.log("hola, Mundo");
+App("#app");
---
+++ 05-todo-app/src/todo/app.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @param {string} elementId
+ */
+export const App = (elementId) => {
+  // Cuando la funcion App() es llamada, el elemento con el id 'app' es renderizado
+  (() => {
+    const app = document.createElement("div");
+    app.innerHTML = `<h1>Hola Mundo</h1>`;
+    document.querySelector(elementId).appendChild(app);
+  })();
+};

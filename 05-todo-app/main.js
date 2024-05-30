42868e7ea66e330739673a1819c0d39d3c148114
Drixner <drixnerrafael1@gmail.com>
2024-05-29 22:35:44 -0500

    Refactor file paths and import statements for todo app

--- 05-todo-app/main.js
+++ 05-todo-app/main.js
@@ -1,4 +1,4 @@
 import "./style.css";
-import { App } from "./src/todo/app";
+import { App } from "./src/todos/app";
 
-App("#app");
+App("#app")
--- 05-todo-app/src/todos/app.js
+++ 05-todo-app/src/todos/app.js
@@ -1,12 +1,13 @@
 /**
  *
- * @param {string} elementId
+ * @param {String} elementId
  */
+
 export const App = (elementId) => {
   // Cuando la funcion App() es llamada, el elemento con el id 'app' es renderizado
   (() => {
     const app = document.createElement("div");
     app.innerHTML = `<h1>Hola Mundo</h1>`;
-    document.querySelector(elementId).appendChild(app);
+    document.querySelector(elementId).append(app);
   })();
 };

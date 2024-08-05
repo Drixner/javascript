ac826addecbbde1647d6170fb65d6698a4a6963f
Drixner <drixnerrafael1@gmail.com>
2024-08-04 22:42:53 -0500

    Refactor todo.store.js and update file paths and import statements for todo app

--- 05-todo-app/src/todos/use-cases/create-todo-html.js
+++ 05-todo-app/src/todos/use-cases/create-todo-html.js
@@ -2,4 +2,13 @@ import { Todo } from "../models/todo.model";
 
 export const createTodoHtml = (todo) => {
     if (!todo) throw new Error("Todo is required");
+    const html = `
+    <!-- <li class="completed" data-id="abc">
+                <div class="view">
+                    <input class="toggle" type="checkbox" checked>
+                    <label>Probar JavaScript</label>
+                    <button class="destroy"></button>
+                </div>
+                <input class="edit" value="Create a TodoMVC template">
+            </li> -->`
 }

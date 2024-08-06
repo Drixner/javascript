413c654bdce3f1dfd415f209374c6fb84661aea5
Drixner <drixnerrafael1@gmail.com>
2024-08-05 21:55:32 -0500

    Refactor create-todo-html.js to remove unnecessary code

--- 05-todo-app/src/todos/use-cases/create-todo-html.js
+++ 05-todo-app/src/todos/use-cases/create-todo-html.js
@@ -4,13 +4,6 @@ export const createTodoHtml = (todo) => {
     if (!todo) throw new Error("Todo is required");
 
     const html =
-        `<li class="completed" data-id="abc">
-                <div class="view">
-                    <input class="toggle" type="checkbox" checked>
-                    <label>Probar JavaScript</label>
-                    <button class="destroy"></button>
-                </div>
-                <input class="edit" value="Create a TodoMVC template">
-            </li>`;
+        ``;
     
 };

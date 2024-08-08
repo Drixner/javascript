e7e09521c2c2f710e157d7ddcc75dbfdd7a29bf9
Drixner <drixnerrafael1@gmail.com>
2024-08-08 00:00:25 -0500

    Refactor create-todo-html.js to remove unnecessary code and improve code readability

--- 05-todo-app/src/todos/use-cases/create-todo-html.js
+++ 05-todo-app/src/todos/use-cases/create-todo-html.js
@@ -2,7 +2,7 @@ import { Todo } from "../models/todo.model";
 
 export const createTodoHtml = (todo) => {
     if (!todo) throw new Error("Todo is required");
-
+ArticuloId
     const html = `<div>
         <h2>${todo.title}</h2>`;
     

7ed62375e8ef54c258fd8e2a1ae66a066803854b
Drixner <drixnerrafael1@gmail.com>
2024-08-18 00:26:04 -0500

    new change

--- 05-todo-app/src/todos/app.js
+++ 05-todo-app/src/todos/app.js
@@ -28,6 +28,7 @@ export const App = (elementId) => {
     document.querySelector(elementId).append(app);
     displayTodos();
   })();
+  // esto es un mensaje de prueba
 
   // Referencias HTML
   const newDescriptionInput = document.querySelector(ElementIDs.NewTodoInput);

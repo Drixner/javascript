0dfcb5df2c0f077670efd9d97fc537a07a11d99d
Drixner <drixnerrafael1@gmail.com>
2024-09-10 01:21:32 -0500

    new change

--- 05-todo-app/src/todos/use-cases/render-pending.js
+++ 05-todo-app/src/todos/use-cases/render-pending.js
@@ -1,19 +1,18 @@
 import todoStore, { Filters } from '../../store/todo.store';
 
 let element;
+
 /**
- * 
- * @param {String} elementId 
+ *
+ * @param {String} elementId
  */
 
+// esto es tambien otro comentario
 // esta constante renderPending es una función que recibe un elementoId, si el elemento no existe, se busca en el DOM, si no existe se lanza un error, si existe se le asigna el valor de la cantidad de tareas pendientes que se obtiene del store de tareas, que se obtiene con el método getTodos, que recibe como parámetro el filtro de tareas pendientes, que se importa del store de tareas.
-export const renderPending = ( elementId ) => {
+export const renderPending = (elementId) => {
+  if (!element) element = document.querySelector(elementId);
 
-    if ( !element ) 
-        element = document.querySelector( elementId );
+  if (!element) throw new Error(`Element ${elementId} not found`);
 
-    if ( !element )
-        throw new Error(`Element ${ elementId } not found`);
-
-    element.innerHTML = todoStore.getTodos( Filters.Pending ).length;
-}
+  element.innerHTML = todoStore.getTodos(Filters.Pending).length;
+};

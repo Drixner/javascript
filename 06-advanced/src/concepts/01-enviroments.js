2e0bdefc82788c1009ecc273d210c85daaef7d33
Drixner <drixnerrafael1@gmail.com>
2024-09-13 19:09:51 -0500

    update

---
+++ 06-advanced/src/concepts/01-enviroments.js
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+
+export const environmentsComponent = (element) => {
+    const env = import.meta.env;
+    console.log(env);
+    const html = `
+            Dev: ${env.DEV} <br/>
+            Prod: ${env.PROD} <br/>
+            Key: ${env.VITE_API_KEY} <br/>
+            URL: ${env.VITE_BASE_URL} <br/>
+    `;
+    element.innerHTML = html;
+
+}
---
+++ 06-advanced/src/concepts/02-callbacks.js
@@ -0,0 +1,25 @@
+import { heroes } from "../data/heroes"
+
+/**
+ * 
+ * @param {element} element 
+ */
+
+export const callbacksComponent = (element) => {
+    const id = '5d86371f2343e37870b91ef1'
+    findHero(id, ({name}) => {
+        element.innerHTML = name;
+    });
+}
+
+
+/**
+ * 
+ * @param {String} id 
+ * @param {(hero:Object) => void} callback 
+ */
+const findHero = (id, callback) => {
+    const hero = heroes.find(hero => hero.id === id);
+
+    callback(hero);
+}

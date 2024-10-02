e6c1cc3d7e4fbedf797150fadc5bd5b095e0cdf2
Drixner <drixnerrafael1@gmail.com>
2024-10-02 00:42:18 -0500

    update

--- 06-advanced/src/concepts/demo.js
+++ 06-advanced/src/concepts/demo.js
@@ -3,7 +3,7 @@
  * @param {HTMLDivElement} element
  */
 
-//Esto sirve para que no se sobreescriban las funciones
+
 export const demoComponent = (element) => {
   console.log("Demo Component");
 };

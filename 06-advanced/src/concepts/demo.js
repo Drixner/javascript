5f85f3aa9da0e2810700205b6d8d0c0e70d89176
Drixner <drixnerrafael1@gmail.com>
2024-10-09 09:39:27 -0500

    update

--- 06-advanced/src/concepts/demo.js
+++ 06-advanced/src/concepts/demo.js
@@ -4,7 +4,7 @@
  */
 
 // demoComponente es una función que recibe un elemento HTMLDivElement y loguea un mensaje en la consola
-
+// esto es un comentario.
 // esto es  otr comentario
 export const demoComponent = (element) => {
   console.log("Demo Component");
